6a63669e8f0436c77254fe5c91a029814f7bba87
Trex275 <73885729+Trex275@users.noreply.github.com>
2021-10-19 16:50:34 +0530

    Add files via upload

---
+++ security.py
@@ -0,0 +1,41 @@
+import cv2
+import dropbox
+import time
+import random
+
+start_time = time.time()
+
+def take_pictures():
+    number = random.randint(0,100)
+    videoCaptureObject = cv2.VideoCapture(0)
+    result = True 
+    while(result):
+        ret, frame = videoCaptureObject.read()
+        image_name = "img" + str(number) + ".png"
+        cv2.imwrite(image_name, frame)
+        start_time = time.time()
+        result = False
+    return image_name
+    print("snapshot taken....")
+    videoCaptureObject.release()
+    cv2.destroyAllWindows()
+
+def upload_pictures(image_name):
+    access_token = "jGkxKdo7KjcAAAAAAAAAAdsK96_nfQ7mheVO7xjfNc61ksY0kUtwZYc-kJaPVjgj"
+    file_from = image_name
+    file_to = "/testFolder/"+(image_name)
+    dbx = dropbox.Dropbox(access_token)
+
+    with open(file_from, 'rb') as f:
+        dbx.files_upload(f.read(), file_to, mode=dropbox.files.WriteMode.overwrite)
+        print("file uploaded!!")
+
+
+def main():
+    while(True):
+        if ((time.time() - start_time) >= 5):
+            name = take_pictures()
+            upload_pictures(name)
+
+main()
+
